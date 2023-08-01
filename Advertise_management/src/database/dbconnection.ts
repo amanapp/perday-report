ecf2919c506979e6c034bd2b122a50aa7f5e8db2
amanapp <137894561+amanapp@users.noreply.github.com>
2023-08-01 23:01:07 +0530

    Delete Advertise_management directory

--- Advertise_management/src/database/dbconnection.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize('adv_test', 'postgres', 'postgres', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-
-export default sequelize;
